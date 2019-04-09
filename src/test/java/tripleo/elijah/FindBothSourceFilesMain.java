a51a9ee0a5f5d542612f867eeaf783c48ceac282
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-09 05:58:49 -0700

    update apr 09

--- src/test/java/tripleo/elijah/FindBothSourceFilesMain.java
+++ src/test/java/tripleo/elijah/FindBothSourceFilesMain.java
@@ -1,17 +1,7 @@
-/*
- * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
- *
- * The contents of this library are released under the LGPL licence v3,
- * the GNU Lesser General Public License text was downloaded from
- * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
- *
- */
 /**
  * 
  */
 package tripleo.elijah;
-
-import java.io.IOException;
 
 import tripleo.elijah.comp.GenBuffer;
 import tripleo.elijah.gen.CompilerContext;
@@ -23,22 +13,11 @@ import tripleo.elijah.gen.CompilerContext;
 public class FindBothSourceFilesMain {
 
 	public static void main(String[] args) {
-		FindBothSourceFiles f=new FindBothSourceFiles(/*"xx"*/);
-		try {
-			f.testParseFile();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		FindBothSourceFiles f=new FindBothSourceFiles("xx");
+		f.testParseFile();
 		GenBuffer gbn = new GenBuffer();
-		CompilerContext cctx = new CompilerContext("fact.elijah");
+		CompilerContext cctx = new CompilerContext();
 		f.factorial_r(cctx , gbn);
-		try {
-			gbn.writeBuffers();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 }
