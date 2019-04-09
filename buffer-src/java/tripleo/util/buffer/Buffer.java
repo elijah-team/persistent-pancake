331e276ad43da44fbc555b65580cffc529265951
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-09 06:41:45 -0700

    some work

--- buffer-src/java/tripleo/util/buffer/Buffer.java
+++ buffer-src/java/tripleo/util/buffer/Buffer.java
@@ -1,23 +1,24 @@
+/**
+ * 
+ */
 package tripleo.util.buffer;
 
-public interface Buffer {
+import tripleo.elijah.util.NotImplementedException;
 
-	void append(String string);
+/**
+ * @author olu
+ *
+ */
+public class Buffer {
 
-	void append_s(String string);
+	public void append(String string) {
+		// TODO Auto-generated method stub
+		NotImplementedException.raise();
+	}
 
-	void append_cb(String string);
-
-	void decr_i();
-	
-	void incr_i();
-	
-	void append_nl_i(String string);
-
-	void append_nl(String string);
-
-	void append_ln(String string);
-
-	String getText();
+	public void append_s(String string) {
+		// TODO Auto-generated method stub
+		NotImplementedException.raise();
+	}
 
 }
