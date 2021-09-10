63c2b970ca5bca8014973259fb01267dc51dc76f
Tripleo envybox <oluoluolu@gmail.com>
2021-09-10 03:09:24 -0400

    make not static

--- src/main/java/tripleo/elijah/stages/deduce/DeduceTypes2.java
+++ src/main/java/tripleo/elijah/stages/deduce/DeduceTypes2.java
@@ -2481,8 +2481,8 @@ public class DeduceTypes2 {
 
 	PromiseExpectations expectations = new PromiseExpectations();
 
-	static class PromiseExpectations {
-		static long counter = 0;
+	class PromiseExpectations {
+		long counter = 0;
 
 		List<PromiseExpectation> exp = new ArrayList<>();
 
