16cc948719d776b657544a06e35774716b5dd49b
Tripleo envybox <oluoluolu@gmail.com>
2021-09-17 06:41:51 -0400

    HACK

--- src/main/java/tripleo/elijah/stages/deduce/DeduceTypes2.java
+++ src/main/java/tripleo/elijah/stages/deduce/DeduceTypes2.java
@@ -1526,7 +1526,8 @@ public class DeduceTypes2 {
 						@Override
 						public void onDone(GeneratedClass result) {
 							genType.node = result;
-							vte.resolveType(genType);
+							if (!vte.typePromise().isResolved()) // HACK
+								vte.resolveType(genType);
 						}
 					});
 				}
