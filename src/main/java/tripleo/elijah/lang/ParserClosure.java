a08476e544ddaae0583960703f7a6bf15a3a69b5
Tripleo Nova <you@figureitout.com>
2022-09-26 18:50:19 -0400

    removals

--- src/main/java/tripleo/elijah/lang/ParserClosure.java
+++ src/main/java/tripleo/elijah/lang/ParserClosure.java
@@ -18,17 +18,9 @@ import org.jetbrains.annotations.NotNull;
 import tripleo.elijah.comp.Compilation;
 
 public class ParserClosure extends ProgramClosure {
-
 	public final OS_Module module;
 
 	public ParserClosure(final String fn, @NotNull final Compilation compilation) {
-/*
-		module = new OS_Module();
-		module.setFileName(fn);
-		module.setParent(compilation); // TODO take a look at all this here
-		compilation.addModule(module, fn);
-*/
-
 		module = compilation.moduleBuilder()
 				.withFileName(fn)
 				.addToCompilation()
@@ -36,12 +28,6 @@ public class ParserClosure extends ProgramClosure {
 	}
 
 	public OS_Package defaultPackageName(final Qualident aPackageName) {
-//		assert module.packageName == null;
-/*
-		module.pushPackageName(aPackageName);
-		return module.parent.makePackage(aPackageName);
-*/
 		return module.pushPackageNamed(aPackageName);
 	}
 }
-
