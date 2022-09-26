e5546260264c8c2bb9df2e161d77c36e1d7af469
Tripleo Nova <you@figureitout.com>
2022-09-26 18:58:51 -0400

    feature: use Pico
    * likely never called

--- src/main/java/tripleo/elijah/Main.java
+++ src/main/java/tripleo/elijah/Main.java
@@ -8,7 +8,12 @@
  */
 package tripleo.elijah;
 
+import org.jetbrains.annotations.NotNull;
+import org.picocontainer.DefaultPicoContainer;
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.PicoContainer;
 import tripleo.elijah.comp.Compilation;
+import tripleo.elijah.comp.ErrSink;
 import tripleo.elijah.comp.IO;
 import tripleo.elijah.comp.StdErrSink;
 
@@ -18,13 +23,45 @@ import java.util.List;
 
 public class Main {
 
-	public static void main(final String[] args) throws Exception {
-		final StdErrSink errSink = new StdErrSink();
-		final Compilation cc = new Compilation(errSink, new IO());
-		final List<String> ls = new ArrayList<String>();
-		ls.addAll(Arrays.asList(args));
-		cc.main(ls, new StdErrSink());
+	public static @NotNull PicoContainer newContainer() {
+		final MutablePicoContainer pico = new DefaultPicoContainer();
+
+		pico.addComponent(PicoContainer.class, pico);
+
+		pico.addComponent(ErrSink.class);
+		pico.addComponent(IO.class);
+		//pico.addComponent(ErrSink.class, new StdErrSink());
+		//pico.addComponent(IO.class, new IO());
+
+		pico.addComponent(Compilation.class);
+
+		//pico.addComponent(ShowInfoWindowAction.class);
+		//pico.addComponent(ShowInfoWindowButton.class);
+
+		return pico;
 	}
+
+	public static void main(final String[] args) throws Exception {
+		final PicoContainer pico = newContainer();
+		final Compilation 	comp = pico.getComponent(Compilation.class);
+		final List<String> 	ls1  = new ArrayList<String>();
+
+		ls1.addAll(Arrays.asList(args));
+
+		comp.main(ls1);
+
+		//
+
+		if (false) {
+			final StdErrSink errSink = new StdErrSink();
+			final Compilation cc = new Compilation(errSink, new IO());
+			final List<String> ls = new ArrayList<String>();
+			ls.addAll(Arrays.asList(args));
+
+			cc.main(ls/*, new StdErrSink()*/);
+		}
+	}
+
 }
 
 //
