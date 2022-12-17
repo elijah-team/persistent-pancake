fc187222c3eed8feafcd558329ae697584c77887
Tripleo Nova <you@youfigureitout.com>
2022-12-17 10:09:51 -0500

    typos

--- src/main/java/tripleo/elijah/nextgen/inputtree/EIT_ModuleInput.java
+++ src/main/java/tripleo/elijah/nextgen/inputtree/EIT_ModuleInput.java
@@ -1,9 +1,14 @@
 package tripleo.elijah.nextgen.inputtree;
 
+import org.jetbrains.annotations.Contract;
 import tripleo.elijah.comp.Compilation;
 import tripleo.elijah.lang.OS_Module;
 
-public class EIT_ModuleInput() {
+public class EIT_ModuleInput {
+    private final OS_Module module;
+    private final Compilation c;
+
+    @Contract(pure = true)
     public EIT_ModuleInput(final OS_Module module, final Compilation c) {
         this.module = module;
         this.c = c;
