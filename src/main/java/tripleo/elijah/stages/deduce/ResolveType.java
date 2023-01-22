8ccf8c2e692a94527638d312ca7668483d89d3da
Tripleo Nova <you@youfigureitout.com>
2023-01-22 07:11:07 -0500

    compare String to String, not IdentExpression

--- src/main/java/tripleo/elijah/stages/deduce/ResolveType.java
+++ src/main/java/tripleo/elijah/stages/deduce/ResolveType.java
@@ -12,7 +12,20 @@ package tripleo.elijah.stages.deduce;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import tripleo.elijah.contexts.ClassContext;
-import tripleo.elijah.lang.*;
+import tripleo.elijah.lang.AliasStatement;
+import tripleo.elijah.lang.ClassStatement;
+import tripleo.elijah.lang.Context;
+import tripleo.elijah.lang.IdentExpression;
+import tripleo.elijah.lang.LookupResultList;
+import tripleo.elijah.lang.NormalTypeName;
+import tripleo.elijah.lang.OS_AnyType;
+import tripleo.elijah.lang.OS_Element;
+import tripleo.elijah.lang.OS_GenericTypeNameType;
+import tripleo.elijah.lang.OS_Module;
+import tripleo.elijah.lang.OS_Type;
+import tripleo.elijah.lang.Qualident;
+import tripleo.elijah.lang.TypeName;
+import tripleo.elijah.lang.TypeOfTypeName;
 import tripleo.elijah.stages.gen_fn.GenType;
 import tripleo.elijah.stages.logging.ElLog;
 import tripleo.elijah.util.NotImplementedException;
@@ -165,7 +178,7 @@ public class ResolveType {
 			case TYPE_OF: {
 				final TypeOfTypeName type_of = (TypeOfTypeName) tn1;
 				final Qualident      q       = type_of.typeOf();
-				if (q.parts().size() == 1 && q.parts().get(0).equals("self")) {
+				if (q.parts().size() == 1 && q.parts().get(0).getText().equals("self")) {
 					assert type_of.getContext() instanceof ClassContext;
 					aR.resolved = ((ClassContext) type_of.getContext()).getCarrier().getOS_Type();
 				}
