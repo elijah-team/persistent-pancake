22ef003c6c70ee2e9eb3be2c03d805bff4961ce6
Tripleo Nova <you@youfigureitout.com>
2023-03-06 02:14:56 -0500

    mar 04 001

--- src/main/java/tripleo/elijah/comp/CCI.java
+++ src/main/java/tripleo/elijah/comp/CCI.java
@@ -21,7 +21,7 @@ class CCI {
 		final ILazyCompilerInstructions cci = mcci.o;
 		final CompilerInstructions      ci  = cci.get();
 
-		tripleo.elijah.util.Stupidity.println_err2("*** " + ci.getName());
+		System.err.println("*** " + ci.getName());
 
 		_cis.onNext(ci);
 		//compilation.pushItem(ci);
