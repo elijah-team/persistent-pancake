763b087fb5bed9b75bfb1f8763ace660b13ff497
Tripleo Nova Szi <szu@tripleo.milkyway>
2023-11-19 03:02:21 -0500

    +t 33: [ClassInstantiationSpec] equivalence to Java
    - except `DebugFlags.classInstantiation2 = true;`

--- src/test/groovy/ClassInstantiationSpec.groovy
+++ src/test/groovy/ClassInstantiationSpec.groovy
@@ -1,47 +1,89 @@
 import spock.lang.Shared
 import spock.lang.Specification
+import tripleo.elijah.comp.Compilation
 import tripleo.elijah.comp.IO
 import tripleo.elijah.comp.StdErrSink
 import tripleo.elijah.factory.comp.CompilationFactory
 
 class ClassInstantiationSpec extends Specification {
-    def "a"() {
-        expect:
-        containsCodeOutput(b) == c
+	def "errorCount"() {
+		expect:
+		errorCount(b) == c
 
-        where:
-        b                               || c
-        "class_instantiation/Bar_105.c" || false
-        "class_instantiation/Bar_103.c" || true
-        "prelude/Prelude/IPrintable.h"  || false
-        "class_instantiation/Bar_102.c" || false
-        "class_instantiation/Bar_105.c" || false
-        "prelude/Prelude/IPrintable.h"  || false
-        "class_instantiation/Bar_102.c" || false
-        "class_instantiation/Bar_103.c" || true
-        "prelude/Prelude/IPrintable.c"  || false
-        "prelude/Prelude/Prelude.h"     || false // --
-        "class_instantiation/Foo.h"     || true
-        "prelude/Prelude/Prelude.c"     || false // --
-        "class_instantiation/Main.h"    || true
-        "prelude/Prelude/ConstString.c" || false
-        "class_instantiation/Foo.c"     || true
-        "class_instantiation/Bar_110.h" || false
-        "class_instantiation/Bar_110.c" || false
-        "class_instantiation/Bar_107.h" || false
-        "class_instantiation/Bar_108.h" || false
-        "class_instantiation/Bar_105.h" || false
-        "class_instantiation/Bar_106.h" || false
-        "class_instantiation/Bar_103.h" || true
-        "class_instantiation/Bar_108.c" || false
-        "class_instantiation/Bar_109.c" || false
-        "class_instantiation/Bar_106.c" || false
-        "class_instantiation/Bar_102.h" || false
-        "class_instantiation/Bar_107.c" || false
-        "prelude/Prelude/ConstString.h" || false
-        "class_instantiation/Main.c"    || true
-        "class_instantiation/Bar_109.h" || false
-    }
+		where:
+		b                                   || c
+		"test/basic1/class_instantiation/"  || 9
+		"test/basic1/class_instantiation2/" || 5
+		"test/basic1/class_instantiation3/" || 8
+	}
+
+	def "codeOutputSize"() {
+		expect:
+		codeOutputSize(b) == c
+
+		where:
+		b                                   || c
+		"test/basic1/class_instantiation/"  || 6
+		"test/basic1/class_instantiation2/" || 6
+		"test/basic1/class_instantiation3/" || 6
+	}
+
+	def "file #a containsCodeOutput #b == #c"() {
+		expect:
+		containsCodeOutput2_(a, b) == c
+
+		where:
+		a                                   | b                                || c
+		"test/basic1/class_instantiation3/" | "class_instantiation3/Foo.h"     || true
+		"test/basic1/class_instantiation3/" | "class_instantiation3/Foo.c"     || true
+		"test/basic1/class_instantiation3/" | "class_instantiation3/Main.h"    || true
+		"test/basic1/class_instantiation3/" | "class_instantiation3/Main.c"    || true
+		"test/basic1/class_instantiation3/" | "class_instantiation3/Bar_103.c" || true
+		"test/basic1/class_instantiation3/" | "class_instantiation3/Bar_103.h" || true
+		"test/basic1/class_instantiation2/" | "class_instantiation2/Foo.h"     || true
+		"test/basic1/class_instantiation2/" | "class_instantiation2/Foo.c"     || true
+		"test/basic1/class_instantiation2/" | "class_instantiation2/Main.h"    || true
+		"test/basic1/class_instantiation2/" | "class_instantiation2/Main.c"    || true
+		"test/basic1/class_instantiation2/" | "class_instantiation2/Bar_103.c" || true
+		"test/basic1/class_instantiation2/" | "class_instantiation2/Bar_103.h" || true
+	}
+
+	def "containsCodeOutput"() {
+		expect:
+		containsCodeOutput(b) == c
+
+		where:
+		b                               || c
+		"class_instantiation/Bar_103.c" || true
+		"class_instantiation/Bar_103.c" || true
+		"class_instantiation/Foo.h"     || true
+		"class_instantiation/Main.h"    || true
+		"class_instantiation/Foo.c"     || true
+		"class_instantiation/Main.c"    || true
+		"class_instantiation/Bar_105.c" || false
+		"prelude/Prelude/IPrintable.h"  || false
+		"class_instantiation/Bar_102.c" || false
+		"class_instantiation/Bar_105.c" || false
+		"prelude/Prelude/IPrintable.h"  || false
+		"class_instantiation/Bar_102.c" || false
+		"prelude/Prelude/IPrintable.c"  || false
+		"prelude/Prelude/Prelude.h"     || false // --
+		"prelude/Prelude/Prelude.c"     || false // --
+		"prelude/Prelude/ConstString.c" || false
+		"class_instantiation/Bar_110.h" || false
+		"class_instantiation/Bar_110.c" || false
+		"class_instantiation/Bar_107.h" || false
+		"class_instantiation/Bar_108.h" || false
+		"class_instantiation/Bar_105.h" || false
+		"class_instantiation/Bar_106.h" || false
+		"class_instantiation/Bar_108.c" || false
+		"class_instantiation/Bar_109.c" || false
+		"class_instantiation/Bar_106.c" || false
+		"class_instantiation/Bar_102.h" || false
+		"class_instantiation/Bar_107.c" || false
+		"prelude/Prelude/ConstString.h" || false
+		"class_instantiation/Bar_109.h" || false
+	}
 
 //    def "maximum of two numbers"() {
 //        expect:
@@ -74,20 +116,21 @@ class ClassInstantiationSpec extends Specification {
 //        new Person(name: "Wilma") || "Female"
 //    }
 
-    def setupSpec() {
-        def f = "test/basic1/class_instantiation/"
+	def setupSpec() {
+		def f = "test/basic1/class_instantiation/"
 
-        def cc = CompilationFactory.mkCompilation(new StdErrSink(), new IO())
-        cc.feedCmdLine([f])
+		def cc = CompilationFactory.mkCompilation(new StdErrSink(), new IO())
+		cc.feedCmdLine([f])
 
-        this.cc = cc
-    }
+		this.cc = cc
+	}
 
-    boolean containsCodeOutput(String c) {
-        cc.reports().containsCodeOutput c
-    }
+	boolean containsCodeOutput(String c) {
+		cc.reports().containsCodeOutput c
+	}
 
-    @Shared cc = null
+	@Shared
+	Compilation cc = null
 
 //    static class Person {
 //        String name
@@ -96,4 +139,27 @@ class ClassInstantiationSpec extends Specification {
 //            name == "Fred" ? "Male" : "Female"
 //        }
 //    }
+	int codeOutputSize(String x) {
+		return cf(x).reports().codeOutputSize()
+	}
+
+	int errorCount(String x) {
+		return cf(x).errorCount()
+	}
+
+	@Shared
+	Map m = new HashMap()
+
+	Compilation cf(String rootCI) {
+		if (!m.containsKey(rootCI)) {
+			def cc = CompilationFactory.mkCompilation(new StdErrSink(), new IO())
+			cc.feedCmdLine([rootCI])
+			m.put(rootCI, cc)
+		}
+		m[rootCI]
+	}
+
+	boolean containsCodeOutput2_(String f, String f2) {
+		cf(f).reports().containsCodeOutput(f2)
+	}
 }
