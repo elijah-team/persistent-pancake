1d76cb13b2f674cdddc39865714bce777ae9d9eb
Tripleo <Tripleo Nova tripleo@Jimothy2.tripleo.tld>
2024-02-12 20:26:31 -0500

    10: Get more Googly

---
+++ src-hard/main/java/tripleo/elijah_durable_pancake/comp/impl/LCM_Event_RootCI.java
@@ -0,0 +1,42 @@
+package tripleo.elijah_durable_pancake.comp.impl;
+
+import tripleo.elijah.ci.CompilerInstructions;
+import tripleo.elijah.comp.i.LCM_CompilerAccess;
+import tripleo.elijah.comp.i.LCM_Event;
+import tripleo.elijah.comp.i.LCM_HandleEvent;
+import tripleo.elijah_durable_pancake.comp.internal.CompilationImpl;
+import tripleo.elijah_pancake.feb24.googly.P;
+
+public class LCM_Event_RootCI implements LCM_Event {
+	private static class LCM_Event_RootCI$ {
+		private static final LCM_Event_RootCI INSTANCE = new LCM_Event_RootCI();
+	}
+
+	private LCM_Event_RootCI() {
+	}
+
+	// TODO 24/01/12 (https://openjdk.org/jeps/8312611) the function was a good call?
+	public static LCM_Event_RootCI instance() {
+		return LCM_Event_RootCI$.INSTANCE;
+	}
+
+	@Override
+	public void handle(final LCM_HandleEvent aHandleEvent) {
+		P.googly(P.CR_START_ROOT, this.getClass().getName());
+
+
+
+		final LCM_CompilerAccess   c      = aHandleEvent.compilation();
+		final CompilerInstructions rootCI = (CompilerInstructions) aHandleEvent.obj();
+
+		try {
+//			c.c().setRootCI(rootCI);
+			c.cr().start(rootCI);
+
+			CompilationImpl cc = (CompilationImpl) c.c();
+			cc.pipelineLogic.dp.checkFinishEventuals();
+		} catch (Exception aE) {
+			aHandleEvent.lcm().exception(aHandleEvent, aE);
+		}
+	}
+}
--- src/main/java/tripleo/elijah_pancake/feb24/googly/P.java
+++ src/main/java/tripleo/elijah_pancake/feb24/googly/P.java
@@ -4,11 +4,17 @@ import tripleo.elijah.lang.OS_Module;
 
 public class P {
 	public static final int PL_ADD_MODULE = 100;
+	public static final int CR_START_ROOT = 101;
 
 	public static void googly(final int code, final Object object) {
 		switch (code) {
 		case PL_ADD_MODULE -> {
 			System.err.println("8787 " + ((OS_Module) object).getFileName());
+		}
+		case CR_START_ROOT -> {
+			//((LCM_Event_RootCI)object).getClass().getName()
+			//final CompilerInstructions rootCI = (CompilerInstructions) aHandleEvent.obj();
+			System.err.println("VVVVV " + (String) object);
 		}
 		}
 	}
